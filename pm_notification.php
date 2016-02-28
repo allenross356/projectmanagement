61cd2811f3fdcac893479cc0b623bac327990636
Allenross356 <allenross653@gmail.com>
2016-02-28 19:32:51 +0530

    populated architecture of notification file

---
+++ pm_notification.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+
+Architecture:
+	Users get notified when:
+		they receive msgs.
+		they receive files.
+		they make money from referrals.
+		their dispute is resolved.
+
+	Coders get notified when:
+		they are awarded a project.
+		they are assigned a project.
+		they have a new project in which they might be interested in.
+		
+	Managers get notified when:
+		they are assigned a project.
+		
+	Employers get notified when:
+		they award a project to someone but he is not available to take it.
+		they want to award a project to someone and mark it for auto-assign or to get notified as soon as he becomes available.
+	
+	Marketers get notified when:
+		they have a new project
+
+
+Interface:
+
+*/
+?>
