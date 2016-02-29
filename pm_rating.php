a99fbefc45fe25315fde5cc0252dafd67453db2b
Allenross356 <allenross653@gmail.com>
2016-02-29 14:28:57 +0530

    29th feb 16, 2:28pm

--- pm_rating.php
+++ pm_rating.php
@@ -8,9 +8,19 @@ Interface:
 
 markProjectImpossible($projectId)
 Permission: Manager
-Description: Once the project is completely paid off or cancelled by the employer, the manager can mark the project impossible. This will give an indication to the system that the project was indeed a hard and technical impossible project. However, the system won't take the manager's feedback for granted, and implement other ways to confirm this.
+Description: The manager can mark the project impossible. This will give an indication to the system that the project was indeed a hard and technical impossible project. However, the system won't take the manager's feedback for granted, and implement other ways to confirm this.
 
 markProjectAbandonded($projectId)
+Permission: Marketer
+Description: The Marketer can mark the project abandoned by employer if the employer doesn't reply for over a threshold period of time. This is different from the system marking the project abandoned automatically with manager's approval after the employer doesn't respond for a threshold period of time and giving the employer several warnings.
+
+markProjectIncomplete($projectId)
+Permission: Marketer
+Description: Marketer can mark a project incomplete by the manager. This is different from the employer cancelling or abandoning the project.
+
+markProjectComplete($projectId)
+Permission: Employer
+Description: Employer can mark the project complete after the entire payment is done. This is different from the system automatically marking the project complete.
 
 */
 
